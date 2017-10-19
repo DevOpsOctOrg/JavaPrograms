28b1b6ddca4e14d61293e15cae7125c9132caad8
Ashakiran7 <32916378+Ashakiran7@users.noreply.github.com>
2017-10-19 19:49:40 -0400

    Commit MyThread
    commiting MyThread

--- MyThread.java
+++ MyThread.java
@@ -3,7 +3,7 @@ package com.training.threads;
 public class MyThread implements  Runnable{
 	
 	public void run(){
-		System.out.println("The Thread started running");
+		
 	}
 	
 	 public static void main(String args[]){
