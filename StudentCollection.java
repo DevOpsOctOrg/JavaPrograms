4f1434493bdd02a99b7d929f290dc9e53648b20c
shanmukhv07 <32942003+shanmukhv07@users.noreply.github.com>
2017-10-20 10:04:05 -0500

    Commiting studentcollection
    Commiting the changes in studentcollection s3 value

--- StudentCollection.java
+++ StudentCollection.java
@@ -6,7 +6,7 @@ public class StudentCollection{
   //Creating user-defined class objects  
   Student s1=new Student(101,"Sonoo",23);  
   Student s2=new Student(102,"Ravi",21);  
-  Student s3=new Student(103,"Hanumat",25);  
+  Student s3=new Student(104,"Hanumat",25);  
   //creating arraylist  
   ArrayList<Student> al=new ArrayList<Student>();  
   al.add(s1);//adding Student class object  
